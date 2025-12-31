324a7e6eb7b204a68123bcd2438abc492fd2be6a
GovindaEkabote <govindaekbote7@gmail.com>
2026-01-01 01:39:45 +0530

    added hello.sh

---
+++ hello.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read num
+
+if [ $num -gt 10 ]; then
+  echo "Greater than 10"
+elif [ $num -eq 10 ]; then
+  echo "Exactly 10"
+else
+  echo "Less than 10"
+fi
+
